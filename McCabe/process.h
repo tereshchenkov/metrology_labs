bcbb9120c5ac534706c7e528aafb1cd4b8b8b732
OksanaPiskunova <piskunova.oksana2014@yandex.ru>
2015-11-12 11:12:56 +0300

    Create process.h

---
+++ McCabe/process.h
@@ -0,0 +1,16 @@
+// process.h - содержит объявления функции для обработки текста
+
+void init_buffer();
+char* get_buffer();
+void add_to_buffer(char[]);
+int process_program();
+void process_function();
+void process_if();
+void process_case();
+void process_for_while();
+void process_repeat();
+int get_next_word(char*);
+void search_include();
+int valid_symbol(char symbol);
+int compare_word(char[]);
+void add_function_name(char[]);

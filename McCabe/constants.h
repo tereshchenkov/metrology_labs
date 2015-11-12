9097ffc3ac50edf2e61d5a888193e762fbdb3106
OksanaPiskunova <piskunova.oksana2014@yandex.ru>
2015-11-12 11:15:35 +0300

    Create constants.h

---
+++ McCabe/constants.h
@@ -0,0 +1,31 @@
+// constants.h - содержит объявление констант
+
+// Константы состояний
+#define WORD 1
+#define SPACE 2
+#define STR 3
+#define ONE_STR_COM 4
+#define COM 5
+// Тип слова
+#define PROCEDURE 0
+#define FUNCTION 1
+#define BEGIN 2
+#define END 3
+#define IF 4
+#define ELSE 5
+#define CASE 6
+#define WHILE 7
+#define REPEAT 8
+#define UNTIL 9
+#define FOR 10
+#define EMPTY 11
+#define OPERATOR_END 12
+#define PROGRAM_END 13
+// Логические константы
+#define YES 1
+#define NO 0
+// Размеры
+#define MAX_WORD_LENGTH 40
+#define MAX_STR_LENGTH 200
+#define MAX_PROGRAM_LENGTH 10000
+#define WORD_COUNT 11
